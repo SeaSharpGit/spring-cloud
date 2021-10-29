f91ce1d3447abe0fc0ad4da86123c37d9e4e631b
wanghai <18912387311@163.com>
2021-10-29 20:03:33 +0800

    init user

--- user/user-biz/src/main/java/com/sea/springcloud/user/UserApplication.java
+++ user/user-biz/src/main/java/com/sea/springcloud/user/UserApplication.java
@@ -1,8 +1,11 @@
 package com.sea.springcloud.user;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class UserApplication {
-
+    public static void main(String[] args) {
+        SpringApplication.run(UserApplication.class, args);
+    }
 }
