fe96c71f67c398ae717fd5485edb59a150acc5a1
wanghai <18912387311@163.com>
2021-12-10 15:26:44 +0800

    省略注解EnableDiscovery

--- gateway/src/main/java/com/sea/springcloud/gateway/GatewayApp.java
+++ gateway/src/main/java/com/sea/springcloud/gateway/GatewayApp.java
@@ -2,10 +2,8 @@ package com.sea.springcloud.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
-@EnableDiscoveryClient
 public class GatewayApp {
     public static void main(String[] args) {
         SpringApplication.run(GatewayApp.class, args);
