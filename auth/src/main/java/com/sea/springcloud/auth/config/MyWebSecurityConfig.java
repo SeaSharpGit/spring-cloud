a55d8278b27b8a0941a4795d26fc91f50dcac020
wanghai <18912387311@163.com>
2021-12-18 22:40:18 +0800

    密码编码器

--- auth/src/main/java/com/sea/springcloud/auth/config/MyWebSecurityConfig.java
+++ auth/src/main/java/com/sea/springcloud/auth/config/MyWebSecurityConfig.java
@@ -1,10 +1,33 @@
 package com.sea.springcloud.auth.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 public class MyWebSecurityConfig extends WebSecurityConfigurerAdapter {
 
+
+
+    /**
+     * 密码编码器
+     * 密码存储格式：{type}encodedPassword
+     * {type}不会造成问题，因为攻击者无需前缀即可识别出来加密类型
+     */
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        //使用默认密码编码器
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+        //使用自定义密码编码器
+//        return new DelegatingPasswordEncoder("bcrypt",
+//                new HashMap<String,PasswordEncoder>(){{
+//                    put("noop", NoOpPasswordEncoder.getInstance());
+//                            put("pbkdf2", new Pbkdf2PasswordEncoder());
+//                            put("scrypt", new SCryptPasswordEncoder());
+//                            put("sha256", new StandardPasswordEncoder());
+//                }});
+    }
 
 }
