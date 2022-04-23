e9a2932773a5abbaed462c6743b0eee12e1bba9c
wanghai <18912387311@163.com>
2022-04-23 16:26:03 +0800

    spring security oauth 注释

--- common/common-security/src/main/java/com/sea/springcloud/common/security/service/MyResourceServerTokenServices.java
+++ common/common-security/src/main/java/com/sea/springcloud/common/security/service/MyResourceServerTokenServices.java
@@ -10,11 +10,15 @@ import org.springframework.security.oauth2.provider.token.store.redis.RedisToken
 
 /**
  * 替换默认实现：{@link RemoteTokenServices}
+ * 实现了自主鉴权，不需要再访问授权服务器，直接从redis中获取数据
  */
 @RequiredArgsConstructor
 public class MyResourceServerTokenServices implements ResourceServerTokenServices {
     private final RedisTokenStore redisTokenStore;
 
+    /**
+     * 鉴权
+     */
     @SneakyThrows
     @Override
     public OAuth2Authentication loadAuthentication(String accessToken) {
